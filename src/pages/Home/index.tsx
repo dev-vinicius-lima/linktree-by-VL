b97a8e845dcdb68ae1d95040007b3299d6f00340
Vinicius Lima <162653125+vinicius-lima-elystech@users.noreply.github.com>
2024-03-13 16:53:37 -0400

    create page home and style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import Social from "../../components/social";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center  ">
+      <h1 className="text-white md:text-4xl text-3xl font-bold mt-20">
+        Linktree by VL
+      </h1>
+      <span className="text-gray-50 mb-5 mt-5">Veja meus links 👇</span>
+      <main className="flex - flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">Wathsapp</p>
+          </a>
+        </section>
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://facebook.com">
+            <FaFacebook size={35} color="#fff" />
+          </Social>
+          <Social url="https://instagram.com">
+            <FaInstagram size={35} color="#fff" />
+          </Social>
+          <Social url="https://youtube.com">
+            <FaYoutube size={35} color="#fff" />
+          </Social>
+        </footer>
+      </main>
     </div>
   );
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex w-full h-screen items-center flex-col">
+      <Link to={"/"}>
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Dev
+          <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+            Link
+          </span>
+        </h1>
+      </Link>
     </div>
   );
 };
