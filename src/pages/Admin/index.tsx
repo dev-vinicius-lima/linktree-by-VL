b2449e3941f686bf16bfa4c337682651e68b712a
Vinicius Lima <162653125+vinicius-lima-elystech@users.noreply.github.com>
2024-03-15 09:47:58 -0400

    registering data to the database

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,17 +1,54 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
 import { FiTrash2 } from "react-icons/fi";
+import { db } from "../../Services/firebaseConecction";
+import {
+  addDoc, // adiconar doc
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const Admin = () => {
   const [nameInput, setNameinput] = useState("");
   const [urlInput, setUrlInput] = useState("");
   const [textColorInput, setTextColorInput] = useState("#f1f1f1");
   const [bgColorInput, setBgColorInput] = useState("#121212");
+
+  function handleRegister(e: FormEvent) {
+    e.preventDefault();
+    if (nameInput === "" || urlInput === "") {
+      alert("preencha todos os campos!");
+      return;
+    }
+    addDoc(collection(db, "links"), {
+      name: nameInput,
+      ulr: urlInput,
+      bg: bgColorInput,
+      color: textColorInput,
+      created: new Date(),
+    })
+      .then(() => {
+        setNameinput("");
+        setUrlInput("");
+        console.log("CADASTRADO COM SUCESSO!");
+      })
+      .catch((erro) => {
+        console.log("Erro ao cadastrar no banco" + erro);
+      });
+  }
+
   return (
     <div className="flex items-center flex-col min-h-screen pb-7 px-2">
       <Header />
-      <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+      <form
+        className="flex flex-col mt-8 mb-3 w-full max-w-xl"
+        onSubmit={handleRegister}
+      >
         <label className="text-white font-medium mt-2 mb-2">
           Nome do Link:
         </label>
