14479667f5f752793f2731e20a420cef10069f13
Vinicius Lima <162653125+vinicius-lima-elystech@users.noreply.github.com>
2024-03-15 13:53:53 -0400

    upgrade color white

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="flex w-full min-h-screen justify-center items-center flex-col">
+    <div className="text-white flex w-full min-h-screen justify-center items-center flex-col">
       <h1 className="font-bold text-6xl mb-6">404</h1>
       <h1 className="font-bold text-4xl mb-4">Ops... Página não encontrada!</h1>
       <p className="italic text-2xl mb-5">
